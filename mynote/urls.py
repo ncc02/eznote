ac0d1193bbb170b087596ac715b06d6723fa4d40
ncc02 <vjtechnologiee.voice@gmail.com>
2023-12-22 22:54:26 +0700

    version2.0

--- mynote/urls.py
+++ mynote/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('notes.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.site_header = "ezNoteAdmin"
+admin.site.site_title = "ezNoteAdmin"
+admin.site.index_title = "Easy to manage your notes online!"
 admin.site.register(Note)
 
+from django.contrib.auth.models import Group
+admin.site.unregister(Group)
